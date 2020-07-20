a01b0107c4455d2407c3c9cb37f529c54b5374b3
Melinda Diaz <melindadiaz@me.com>
2020-07-20 21:59:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index do |dwarves, index|
-    puts dwarves
+  array.each_with_index do |index, dwarves|
+    puts index 
   end
   
   
