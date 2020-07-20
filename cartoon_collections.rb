6da050b6553e10f5c188681c16baf8817862cafb
Melinda Diaz <melindadiaz@me.com>
2020-07-20 21:49:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   
   array.each do |character|
-    puts "Hello ${character}!"
+    puts "Hello #{character}!"
   end
 end
 
